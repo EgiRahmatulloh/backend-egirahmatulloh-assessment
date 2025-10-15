40edbfeab5e3d08b7b4146f25a1b3313aac53a7e
EgiRahmatulloh <egirahmatulloh13@gmail.com>
2025-10-15 17:15:13 +0700

    refactor: tambahkan unggah avatar pengguna

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import multer from 'multer';
 import { authenticateToken } from '../middleware/auth.js';
 import { updateUserProfile } from '../controllers/userController.js';
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 // All user routes will be protected
 router.use(authenticateToken);
 
-router.put('/me', updateUserProfile);
+router.put('/me', upload.single('avatar'), updateUserProfile);
 
 export default router;
